c0bc67c26a53bbe3ebbaf01015fbf0bd32afb310
MalazanThrone <114039823+MalazanThrone@users.noreply.github.com>
2023-09-20 00:30:29 -0700

    updated bookData.js to use API key and increased specificity of error catching

--- src/components/bookData.js
+++ src/components/bookData.js
@@ -2,12 +2,14 @@ import axios from 'axios';
 
 const FetchBookData = async (id) => {
     const API_KEY = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
+    const MAX_RETRIES = 3;
+    let retries = 0;
 
     const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-    while (true) {
+    while (retries < MAX_RETRIES) {
         try {
-            const response = await axios.get(`https://www.googleapis.com/books/v1/volumes/${id}`);
+            const response = await axios.get(`https://www.googleapis.com/books/v1/volumes/${id}&key${API_KEY}`);
             const data = response.data.volumeInfo;
             return {
                 author: data.authors,
@@ -17,12 +19,14 @@ const FetchBookData = async (id) => {
 
         } catch (error) {
             if (error.response && error.response.status === 429) {
-                await delay(1000);
-            } else {
-                throw error;
+                return { error: "Rate limit exceeded" };
             }
+            retries++;
+            await delay(1000);
         }
     }
+
+    return { error: "Unable to acquire data" };
 
 };
 
