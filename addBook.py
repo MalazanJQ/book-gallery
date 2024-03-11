9bbf00e584d385b74bf48d5e4259ab770c5f6bc8
MalazanThrone <114039823+MalazanThrone@users.noreply.github.com>
2024-03-10 21:27:35 -0700

    fixed image downloading for addBook.py

--- addBook.py
+++ addBook.py
@@ -23,12 +23,15 @@ def add_book(year):
                 index += 1
                 show_book_details(book_data, index)
                 correct = input("Is this the correct book?\n")
+                
             
             if year != "current":
                 rating = input(f"What rating would you give {title}?\n")
             official_title = book_data[index]['volumeInfo']['title']
             id = book_data[index]['id']
             image_name = f"/images/{id}.jpg"
+
+            get_image(id)
 
             if year != "current":
                 data.append({
@@ -56,11 +59,26 @@ def show_book_details(data, index):
     print(current['title'])
     print(current['authors'])
 
+def get_image(id):
+    url = f"https://www.googleapis.com/books/v1/volumes/{id}?fields=volumeInfo(imageLinks)"
+    response = requests.get(url)
+    response_json = response.json()
+    image_url = list(response_json.get('volumeInfo', {}).get('imageLinks', {}).values())[-1]
+
+    image = requests.get(image_url).content
+    f = open(f'./public/images/{id}.jpg', 'wb')
+    f.write(image)
+    f.close()
+
+    image = Image.open(f'./public/images/{id}.jpg')
+    image.show()
+
 if __name__ == "__main__":
     year = input("Enter the year or 'Currently Reading' to add a book to your book list\n")
     file = f"C:/Users/shini/OneDrive/Github/book-gallery/src/data/{year}.json"
     if not os.path.exists(file):
-        if input("Do you want to add the new year " + year + "? ").lower() == "yes":
+        ans = input("Do you want to add the new year " + year + "? ").lower()
+        if ans == "yes":
             file = open(f"C:/Users/shini/OneDrive/Github/book-gallery/src/data/{year}.json", "w")
         with open(file, 'w') as json_file:
             json.dump([], json_file, indent=4)
